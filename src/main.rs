7e1a3e1ca43560cf72da504cf7b334474cefb87e
cadaverine <kennville@gmail.com>
2018-08-28 12:56:49 +0300

    add 'get_k_combinations_number' function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+fn get_k_combinations_number(n: u64, k: u64) -> u64 {
+    assert!(n >= k);
+
+    if n == k {
+        return 1;
+    }
+
+    let mut numeratorArray = arr[1..k];
+
+}
+
+
+fn k_combinations_generate(n: u64, k: u64) -> u64 {
+
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    print!("{}", (1..20));
 }
